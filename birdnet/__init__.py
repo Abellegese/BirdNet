1cca937746fd6eb9a4ed6ab09195a8e2c3008292
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2024-02-10 14:41:39 -0800

    Add files via upload

---
+++ birdnet/__init__.py
@@ -0,0 +1,7 @@
+from .annotation_reader import *
+from .dataset import *
+from .model import *
+from .predict import *
+from .preprocessing import *
+from .train import *
+from .utils import *
---
+++ dataset/ReadMe
@@ -0,0 +1,4 @@
+## Dataset for the trainig
+ The dataset was collected from the Intaka Island
+
+ You can find it on the drive
